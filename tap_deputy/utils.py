8277cdcc2eb14cba4f0c43ebc88356ada49b6558
RushiT0122 <rtodkar@stitchdata-talend.com>
2022-09-26 19:44:00 +0000

    - update utility functions

---
+++ tap_deputy/utils.py
@@ -0,0 +1,28 @@
+from typing import Dict
+import json
+from singer import get_logger
+
+LOGGER = get_logger()
+
+def read_config(config_path) -> Dict:
+    """
+    Performs read on the provided filepath,
+    returns empty dict if invalid path provided
+    """
+    try:
+        with open(config_path,'r') as tap_config:
+            return json.load(tap_config)
+    except FileNotFoundError as _:
+        LOGGER.fatal("Failed to load config in dev mode")
+        return {}
+
+
+def write_config(config_path,data :Dict) -> Dict:
+    """
+    Updates the provided filepath with json format of the `data` object
+    """
+    config =  read_config(config_path)
+    config.update(data)
+    with open(config_path,'w') as tap_config:
+        json.dump(config, tap_config, indent=2)
+    return config
