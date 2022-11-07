a709ad6e6a7c9dcfee4d19ce2b9c325fddf4e13a
RushiT0122 <rtodkar@stitchdata-talend.com>
2022-11-07 10:58:55 +0000

    update latest singer-python version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='tap-deputy',
       install_requires=[
           'backoff==1.8.0',
           'requests==2.21.0',
-          'singer-python==5.12.2'
+          'singer-python==5.13.0'
       ],
       extras_require= {
           'dev': [
