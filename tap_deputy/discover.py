8a875e5a1f95b44a980b0201ca0d58b393d941d6
Pagadala Narender <ext.pagadala.narender@globallogic.com>
2025-10-31 13:33:20 +0530

    changes for the metadata

--- tap_deputy/discover.py
+++ tap_deputy/discover.py
@@ -1,64 +1,64 @@
 from singer.catalog import Catalog, CatalogEntry, Schema
 
 RESOURCES = {
-    'Address': 'addresses',
-    'Category': 'categories',
-    'Comment': 'comments',
-    'Company': 'companies',
-    'CompanyPeriod': 'company_periods',
-    'Contact': 'contacts',
-    'Country': 'countries',
-    'CustomAppData': 'custom_app_data',
-    'CustomField': 'custom_fields',
-    'CustomFieldData': 'custom_field_data',
-    'Employee': 'employees',
-    'EmployeeAgreement': 'employee_agreements',
-    'EmployeeAgreementHistory': 'employee_agreement_history',
-    'EmployeeAppraisal': 'employee_appraisal',
-    'EmployeeAvailability': 'employee_availability',
-    'EmployeeHistory': 'employee_history',
-    'EmployeePaycycle': 'employee_paycycles',
-    'EmployeePaycycleReturn': 'employee_paycycle_returns',
-    'EmployeeRole': 'employee_roles',
-    'EmployeeSalaryOpunitCosting': 'employee_salary_opunit_costing',
-    'EmployeeWorkplace': 'employee_workplaces',
-    'EmploymentCondition': 'employment_conditions',
-    'EmploymentContract': 'employee_contracts',
-    'EmploymentContractLeaveRules': 'employee_contract_leave_rules',
-    'Event': 'events',
-    'Geo': 'geo',
-    'Journal': 'journal',
-    'Kiosk': 'kiosks',
-    'Leave': 'leaves',
-    'LeaveAccrual': 'leave_accruals',
-    'LeavePayLine': 'leave_pay_lines',
-    'LeaveRules': 'leave_rules',
-    'Memo': 'memos',
-    'OperationalUnit': 'operational_units',
-    'PayPeriod': 'pay_periods',
-    'PayRules': 'pay_rules',
-    'PublicHoliday': 'public_holidays',
-    'Roster': 'rosters',
-    'RosterOpen': 'roster_opens',
-    'RosterSwap': 'roster_swaps',
-    'SalesData': 'sales_data',
-    'Schedule': 'schedules',
-    'SmsLog': 'sms_logs',
-    'State': 'states',
-    'StressProfile': 'stress_profiles',
-    'SystemUsageBalance': 'system_usage_balances',
-    'SystemUsageTracking': 'system_usage_tracking',
-    'Task': 'tasks',
-    'TaskGroup': 'task_groups',
-    'TaskGroupSetup': 'task_group_setups',
-    'TaskOpunitConfig': 'task_opunit_configs',
-    'TaskSetup': 'task_setups',
-    'Team': 'teams',
-    'Timesheet': 'timesheets',
-    'TimesheetPayReturn': 'timesheet_pay_returns',
-    'TrainingModule': 'training_modules',
-    'TrainingRecord': 'training_records',
-    'Webhook': 'webhooks'
+    'Address': {"stream": 'addresses',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'Category': {"stream": 'categories',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'Comment': {"stream": 'comments',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'Company': {"stream": 'companies',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'CompanyPeriod': {"stream": 'company_periods',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'Contact': {"stream": 'contacts',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'Country': {"stream": 'countries',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'CustomAppData': {"stream": 'custom_app_data',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'CustomField': {"stream": 'custom_fields',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'CustomFieldData': {"stream": 'custom_field_data',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'Employee': {"stream": 'employees',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'EmployeeAgreement': {"stream": 'employee_agreements',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'EmployeeAgreementHistory': {"stream": 'employee_agreement_history',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'EmployeeAppraisal': {"stream": 'employee_appraisal',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'EmployeeAvailability': {"stream": 'employee_availability',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'EmployeeHistory': {"stream": 'employee_history',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'EmployeePaycycle': {"stream": 'employee_paycycles',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'EmployeePaycycleReturn': {"stream": 'employee_paycycle_returns',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'EmployeeRole': {"stream": 'employee_roles',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'EmployeeSalaryOpunitCosting': {"stream": 'employee_salary_opunit_costing',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'EmployeeWorkplace': {"stream": 'employee_workplaces',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'EmploymentCondition': {"stream": 'employment_conditions',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'EmploymentContract': {"stream": 'employee_contracts',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'EmploymentContractLeaveRules': {"stream": 'employee_contract_leave_rules',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'Event': {"stream": 'events',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'Geo': {"stream": 'geo',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'Journal': {"stream": 'journal',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'Kiosk': {"stream": 'kiosks',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'Leave': {"stream": 'leaves',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'LeaveAccrual': {"stream": 'leave_accruals',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'LeavePayLine': {"stream": 'leave_pay_lines',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'LeaveRules': {"stream": 'leave_rules',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'Memo': {"stream": 'memos',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'OperationalUnit': {"stream": 'operational_units',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'PayPeriod': {"stream": 'pay_periods',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'PayRules': {"stream": 'pay_rules',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'PublicHoliday': {"stream": 'public_holidays',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'Roster': {"stream": 'rosters',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'RosterOpen': {"stream": 'roster_opens',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'RosterSwap': {"stream": 'roster_swaps',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'SalesData': {"stream": 'sales_data',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'Schedule': {"stream": 'schedules',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'SmsLog': {"stream": 'sms_logs',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'State': {"stream": 'states',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'StressProfile': {"stream": 'stress_profiles',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'SystemUsageBalance': {"stream": 'system_usage_balances',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'SystemUsageTracking': {"stream": 'system_usage_tracking',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'Task': {"stream": 'tasks',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'TaskGroup': {"stream": 'task_groups',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'TaskGroupSetup': {"stream": 'task_group_setups',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'TaskOpunitConfig': {"stream": 'task_opunit_configs',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'TaskSetup': {"stream": 'task_setups',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'Team': {"stream": 'teams',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'Timesheet': {"stream": 'timesheets',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'TimesheetPayReturn': {"stream": 'timesheet_pay_returns',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'TrainingModule': {"stream": 'training_modules',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'TrainingRecord': {"stream": 'training_records',"forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]},
+    'Webhook': {"stream": 'webhooks', "forced-replication-method":"INCREMENTAL", "valid-replication-keys": ["Modified"]}
 }
 
 TYPE_MAP = {
@@ -70,7 +70,7 @@ TYPE_MAP = {
     'Time': 'string'
 }
 
-def get_schema(client, resource_name):
+def get_schema(client, resource_name, resource_data):
     data = client.get(
         '/api/v1/resource/{}/INFO'.format(resource_name),
         endpoint='resource_info')
@@ -80,7 +80,9 @@ def get_schema(client, resource_name):
         {
             'breadcrumb': [],
             'metadata': {
-                'tap-deputy.resource': resource_name
+                'tap-deputy.resource': resource_name,
+                'forced-replication-method' : resource_data['forced-replication-method'],
+                'valid-replication-keys' : resource_data['valid-replication-keys'],
             }
         }
     ]
@@ -120,8 +122,8 @@ def get_schema(client, resource_name):
 def discover(client):
     catalog = Catalog([])
 
-    for resource_name in RESOURCES.keys():
-        schema_dict, metadata = get_schema(client, resource_name)
+    for resource_name, resource_data in RESOURCES.items():
+        schema_dict, metadata = get_schema(client, resource_name, resource_data)
         schema = Schema.from_dict(schema_dict)
 
         stream_name = RESOURCES[resource_name]
